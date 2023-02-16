2ab1986378d2d065b03662a2987e0f9744217fce
Farnabaz <farnabaz@gmail.com>
2023-02-16 14:56:10 +0100

    Create Comp.js

---
+++ Comp.js
@@ -0,0 +1,8 @@
+const __sfc__ = {}
+import { openBlock as _openBlock, createElementBlock as _createElementBlock } from "vue"
+function render(_ctx, _cache) {
+  return (_openBlock(), _createElementBlock("div", null, " test "))
+}
+__sfc__.render = render
+__sfc__.__file = "Comp.vue"
+export default __sfc__
